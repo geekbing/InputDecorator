a15ddcc00bd6201409943af85ee6b4587ee1d234
bing <dhuzbb@gmail.com>
2017-03-03 14:27:58 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int c;
+
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
